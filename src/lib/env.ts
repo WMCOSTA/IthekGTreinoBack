ae8e182c41571da3b0a50ea7ee6d5622c0ce55f0
unknown <wcosta1506@gmail.com>
2026-03-29 20:40:08 -0300

    Gerando prisma migrations e correção da variavel de ambiente para login na tela principal fit-ia

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -10,9 +10,10 @@ const envSchema = z.object({
   API_BASE_URL: z.url().default("http://localhost:8080"),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
-  GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
+  //GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
   OPENAI_API_KEY: z.string().optional(),
-  WEB_APP_BASE_URL: z.url(),
+  //WEB_APP_BASE_URL: z.url(),
+  WEB_APP_BASE_URL: ("https://gtreino.online"),
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
